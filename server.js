a11f5c4a9ae5ee55ea1ed4ba8400ac748b68bc66
drsde <dexterviper7739@gmail.com>
2024-07-20 01:20:15 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const sendMessage = require('./api/send_message');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/api', sendMessage);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
